a31f816a23984bbaf262867357a2a8693f6b3c22
Xingyu <xingyu3906@gmail.com>
2022-09-28 10:10:29 +0800

    changes

---
+++ Exercise/Ex03_tasks.R
@@ -0,0 +1,5 @@
+# 1.The Iris data was introduced in Lecture 1. Import it into R. This and the subsequent
+# tasks will do some analysis bases on this dataset.
+#
+# 1.1 Insert a column named 'sepal_width_level' in the iris data, showing whether
+#  petal length is greater than 4. Two values in this column "< 4", ">= 4".
